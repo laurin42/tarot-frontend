5e5fa709a6a708feb605fd3897378ae12288296e
laurin42 <laurin.schmidt@yahoo.com>
2025-04-02 11:46:08 +0200

    Initial commit: Frontend project setup

--- app/(auth)/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import AuthScreen from "../../components/auth/AuthScreen";
-
-export default function Auth(): JSX.Element {
-  return <AuthScreen />;
-}
--- components/CardStack/index.tsx
+++
@@ -1,2 +0,0 @@
-export { default } from "./CardStackView";
-export * from "./CardStackView";
